fddd28f09af430b1fb0f9bc039001ec386637f0b
Ants_double <ants_double@yeah.net>
2020-08-13 21:55:31 +0800

    login

--- src/main/java/com/ants/orchid/controller/UserController.java
+++ src/main/java/com/ants/orchid/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ants.orchid.pojo.domain.UserPo;
 import com.ants.orchid.server.api.UserServer;
 import com.antsdouble.util.Result;
 import javafx.util.Pair;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,22 @@ public class UserController {
     public Result getUserById(@RequestParam(value = "name")String name){
         UserPo userByName = userServer.getUserByName(name);
         return  Result.success(userByName);
+    }
+    @GetMapping(value = "login")
+    public Result login(@RequestParam(value = "name",required = true)String name,
+                        @RequestParam(value = "password",required = true)String password){
+        try {
+            UserPo userByName = userServer.getUserByName(name);
+            if (StringUtils.equals(password,userByName.getPassword())){
+                return  Result.success("login success");
+            }else {
+                return  Result.error("password error");
+            }
+        }
+        catch (Exception ex){
+            return  Result.error("network exception");
+        }
+
+
     }
 }
